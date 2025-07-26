e7552111c8dfd3f3a2f6223a1ef33d49f652b403
Gabriel D'Nillo <56946465+NilloGabriel@users.noreply.github.com>
2025-07-26 15:53:09 -0300

    add saida view

--- database/democsv.sql
+++ database/democsv.sql
@@ -1,8 +1,11 @@
 DROP DATABASE IF EXISTS democsv;
+
 CREATE DATABASE IF NOT EXISTS democsv;
+
 USE democsv;
 
 DROP TABLE IF EXISTS uf;
+
 CREATE TABLE IF NOT EXISTS uf (
   id INT NOT NULL AUTO_INCREMENT,
   sigla VARCHAR(2),
@@ -11,6 +14,7 @@ CREATE TABLE IF NOT EXISTS uf (
 );
 
 DROP TABLE IF EXISTS cidade;
+
 CREATE TABLE IF NOT EXISTS cidade (
   id INT NOT NULL AUTO_INCREMENT,
   nome VARCHAR(255),
@@ -21,12 +25,11 @@ CREATE TABLE IF NOT EXISTS cidade (
   cod_siafi VARCHAR(255),
   uf_id INT,
   PRIMARY KEY (id),
-  CONSTRAINT fk_cidade_uf1
-    FOREIGN KEY (uf_id)
-    REFERENCES uf (id)
+  CONSTRAINT fk_cidade_uf1 FOREIGN KEY (uf_id) REFERENCES uf (id)
 );
 
 DROP TABLE IF EXISTS empresa;
+
 CREATE TABLE IF NOT EXISTS empresa (
   id INT NOT NULL AUTO_INCREMENT,
   slug VARCHAR(255),
@@ -37,7 +40,24 @@ CREATE TABLE IF NOT EXISTS empresa (
   porte INT,
   cidade_id INT,
   PRIMARY KEY (id),
-  CONSTRAINT fk_empresa_cidade
-    FOREIGN KEY (cidade_id)
-    REFERENCES cidade (id)
+  CONSTRAINT fk_empresa_cidade FOREIGN KEY (cidade_id) REFERENCES cidade (id)
 );
+
+DROP VIEW IF EXISTS saida;
+
+CREATE
+OR REPLACE VIEW saida AS
+SELECT
+  e.nome_fantasia,
+  e.slug,
+  e.dt_inicio_atividade AS inicio_atividades,
+  e.porte AS porte_empresa,
+  c.nome AS nome_cidade,
+  u.sigla AS sigla_uf,
+  c.populacao AS populacao_cidade,
+  c.latitude AS latitude_cidade,
+  c.longitude AS longitude_cidade
+FROM
+  empresa AS e
+  INNER JOIN cidade AS c ON (e.cidade_id = c.id)
+  INNER JOIN uf AS u ON (c.uf_id = u.id)
