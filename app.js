19ba400a7454d120bedb66b1f3612fb30acc7b14
Gabriel D'Nillo <56946465+NilloGabriel@users.noreply.github.com>
2025-07-26 15:53:12 -0300

    check if saida.csv file exists

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { existsSync } from 'fs'
+
 import { allPromises } from './src/input.js'
 import debug from 'debug'
 
@@ -5,12 +7,19 @@ const log = debug('app:democsv')
 
 console.time('demo-csv')
 
+const fileExists = existsSync('database/csv/output/saida.csv')
+
 log(`processing all promises`)
 const ONE_SECOND = 1000
 setInterval(() => process.stdout.write('.'), ONE_SECOND).unref()
 ;(async function run() {
   try {
-    await allPromises
+    if (!fileExists) {
+      await allPromises
+      return
+    }
+    log(`saida.csv file already exists`)
+    process.exit(1)
   } catch (err) {
     throw err
   } finally {
