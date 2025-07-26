251c8d35dbed94089a75b280a427a0e8ddeecda4
Gabriel D'Nillo <56946465+NilloGabriel@users.noreply.github.com>
2025-07-26 15:53:11 -0300

    simulate progress bar

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 import { allPromises } from './src/input.js'
+
+const ONE_SECOND = 1000
+setInterval(() => process.stdout.write('.'), ONE_SECOND).unref()
 ;(async function run() {
   try {
     await allPromises
--- src/output.js
+++ src/output.js
@@ -23,10 +23,4 @@ export const output = async () => {
       reject(err)
     }
   })
-    .then(() => {
-      console.log('Arquivo saida.csv gerado com sucesso!')
-    })
-    .catch(() => {
-      console.log('Não foi possível gerar o arquivo de saída.')
-    })
 }
