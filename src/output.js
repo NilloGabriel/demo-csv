f49b1966d5a54158630e5b73787a2eca457d3c42
Gabriel D'Nillo <56946465+NilloGabriel@users.noreply.github.com>
2025-07-26 15:53:15 -0300

    removing promise and renaming the function output

--- src/output.js
+++ src/output.js
@@ -3,24 +3,21 @@ import { createWriteStream } from 'fs'
 import { connection } from '../database/database.js'
 import csv from 'fast-csv'
 
-export const output = async () => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const filename = 'database/csv/output/saida.csv'
-      const writableStream = createWriteStream(filename)
+export const outputData = async () => {
+  try {
+    const filename = 'database/csv/output/saida.csv'
+    const writableStream = createWriteStream(filename)
 
-      await connection
-        .query(`SELECT * FROM saida`)
-        .then(([rows]) => {
-          const data = JSON.parse(JSON.stringify(rows))
-          csv.write(data, { headers: true }).pipe(writableStream)
-        })
-        .catch(err => {
-          throw err
-        })
-      resolve()
-    } catch (err) {
-      reject(err)
-    }
-  })
+    await connection
+      .query(`SELECT * FROM saida`)
+      .then(([rows]) => {
+        const data = JSON.parse(JSON.stringify(rows))
+        csv.write(data, { headers: true }).pipe(writableStream)
+      })
+      .catch(err => {
+        throw err
+      })
+  } catch (err) {
+    throw err
+  }
 }
